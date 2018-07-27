3a5b53a9023e383abbacea3d1911fcf8a28c67cf
Anna <annanowaczykmail@gmail.com>
2018-07-28 00:54:37 +0200

    Set queryBooks method for Searchpage

--- src/Searchpage.js
+++ src/Searchpage.js
@@ -2,3 +2,28 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import Book from './Book'
 import * as BooksAPI from './BooksAPI'
+
+class Searchpage extends Component {
+  state = {
+    query: "",
+    queriedBooks: []
+  };
+
+  queryBooks = query => {
+    const queriedBooks = [];
+
+    if (query) {
+      let queryResults = [];
+      BooksAPI.search(query).then (results => {
+        if (results && results.length) {
+          queryResults = results.map(result => {
+            result.shelf = this.addShelf(result);
+            return result;
+          });
+          
+        }
+      })
+    }
+
+  }
+}
