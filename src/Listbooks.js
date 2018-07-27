a75c1f9a810f70329e0ff71c32acfc3e7cfdb2b1
Anna <annanowaczykmail@gmail.com>
2018-07-28 00:33:23 +0200

    Set markup for a booklist in Listbooks component

--- src/Listbooks.js
+++ src/Listbooks.js
@@ -13,6 +13,38 @@ class Listbooks extends Component {
     const read = books.filter(book => book.shelf === 'read');
     const wantToRead = books.filter(book => book.shelf === 'wantToRead');
 
+    return (
+      <div className = "list-books">
+        <div className = "list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className = "list-books-content">
+          <div>
+            <div className = "bookshelf">
+              <h2 className = "bookshelf-title">Currently Reading</h2>
+              {currentlyReading.length > 0 &&
+              <Book
+              filteredBooks={currentlyReading}
+              changeShelf={this.props.changeShelf}
+            />
+            }
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              {read.length > 0 &&
+              <Book
+              filteredBooks={wantToRead}
+              changeShelf={this.props.changeShelf}
+            />
+            }
+            </div>
+            <div className
+          </div>
+        </div>
+      </div>
+
+    )
+
 
   }
 }
